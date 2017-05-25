2021ca5ce9d80e10510ed43e8a9740f73fefd326
dana <igra.dana@gmail.com>
2017-05-25 16:16:38 +0300

    release 1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const   express = require('express'),
 		bodyParser = require('body-parser'),
 		mod = require('./module.js'),
@@ -30,7 +29,6 @@ app.all('*',
 app.post('/getSeriesData/',
 	(req,res) => {
 	var sID = req.body.id;
-	//var data = mod.getSeriesData(sID);
 	mod.getSeriesData(sID).then((serieses) => {
 	        if(serieses.length)
 	        	res.json(serieses);
@@ -39,12 +37,6 @@ app.post('/getSeriesData/',
 		}, (error) => {
 		    console.log(error);
 	    });
-	// if(data == 0){
-	// 	res.status(200).json({"err":"no such series"});
-	// }
-	// else{
-	// 	res.status(200).json(data);
-	// }
 });
 
 app.put('/getSeriesByEndDate',
@@ -59,73 +51,8 @@ app.put('/getSeriesByEndDate',
 		}, (error) => {
 		    console.log(error);
 	    });
-		// data = mod.getSeriesByEndDate(day,month);
-
-	// if(data == 0) {
-	// 	res.status(200).json({"err":"no such series"});
-	// }
-	// else {
-	// 	res.status(200).json(data);
-	// }
 });
 app.listen(process.env.PORT || 3000)
 //app.listen(port, 
 //	()=> {
 //	console.log(`listening on port ${port}`);
-=======
-const   express = require('express'),
-		bodyParser = require('body-parser'),
-		mod = require('./module.js'),
-		app = express(),
-		port = process.env.port || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.all('*',
-	(req, res, next) => {
-		console.log("runs for all HTTP verbs first");
-		next();
-	});
-
-app.get('/',
-	(req,res) =>{
-	res.sendfile('./index.html');
-});
-
-app.get('/getAllSerieses',
-	(req,res) => {
-	res.json(mod.getAllSerieses());
-});
-
-app.post('/getSeriesData/',
-	(req,res) => {
-	var sID = req.body.id;
-	var data = mod.getSeriesData(sID);
-	if(data == 0){
-		res.status(200).json({"err":"no such series"});
-	}
-	else{
-		res.status(200).json(data);
-	}
-});
-
-app.put('/getSeriesByEndDate',
-	(req,res) => {
-	var day = req.body.day,
-		month = req.body.month,
-		data = mod.getSeriesByEndDate(day,month);
-
-	if(data == 0) {
-		res.status(200).json({"err":"no such series"});
-	}
-	else {
-		res.status(200).json(data);
-	}
-});
-app.listen(process.env.PORT || 3000)
-//app.listen(port, 
-//	()=> {
-//	console.log(`listening on port ${port}`);
->>>>>>> 4d0b2c11c1eb7409ef567fb69dab6ecdc02976e3
-//});
--- module.js
+++ module.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const data = require("./data/serieses.json");
 
 const mongoose = require('mongoose'),
@@ -27,46 +26,3 @@ exports.getSeriesData = function(sID) {
 
 exports.getSeriesByEndDate = function(day,month) {
     return app.find({day:day, month:{$gt:month}});
-=======
-const data = require("./data/serieses.json");
-
-exports.getAllSerieses = function() {
-    return data;
-}
-
-exports.getSeriesData = function(sID) {
-    let findSeries = false;
-    for(let i in data.serieses){
-        var series = data.serieses[i];
-        if(series.id == sID){
-            console.log(`${data.serieses[i].name} has been found`);
-            findSeries = true;
-            return series;
-        }
-    }
-    return 0;
-}
-
-exports.getSeriesByEndDate = function(day,month) {
-    var returnData = '[';
-    let findData = false;
-    for(let i in data.serieses){
-        var series = data.serieses[i];
-        if(series.day == day && series.month >= month){
-            console.log(`${data.serieses[i].name} has been found`);
-            findData = true;
-            returnData += JSON.stringify(series);
-            returnData += `,`;
-        }
-    }
-    if(!findData){
-        return 0;
-    }
-    else {
-        returnData = returnData.substr(0,returnData.length-1);
-        returnData += ']';
-        returnData = JSON.parse(returnData);
-        return returnData;
-    }
->>>>>>> 4d0b2c11c1eb7409ef567fb69dab6ecdc02976e3
-}
